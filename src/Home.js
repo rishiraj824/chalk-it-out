a5ab55b2dc22ad11637e4f9773a6b9fdaa06b574
Rishi Raj <rishiraj824@gmail.com>
2020-05-30 17:26:17 -0400

    prettier 😬

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useRef, useState } from "react";
-import { useHistory } from "react-router-dom";
-import Button from "./components/button";
-import Input from "./components/input";
-import "./Home.css";
-import Layout from "./Layout";
+import React, { useEffect, useRef, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Button from './components/button';
+import Input from './components/input';
+import './Home.css';
+import Layout from './Layout';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
 const Home = () => {
-  const [lectureName, setState] = useState("");
+  const [lectureName, setState] = useState('');
   const history = useHistory();
   let inputRef = useRef(null);
 
@@ -19,13 +19,13 @@ const Home = () => {
   const createLiveStream = async () => {
     try {
       const result = await fetch(API_URL, {
-        method: "post",
+        method: 'post',
         body: JSON.stringify({
-          user: document.cookie.get("oauth"),
+          user: document.cookie.get('oauth'),
         }),
       });
       history.push(
-        `/teach/${lectureName}-${result.stream_key}?id=${result.id}`
+        `/teach/${lectureName}-${result.stream_key}?id=${result.id}`,
       );
     } catch (err) {
       console.log(err);
@@ -34,13 +34,13 @@ const Home = () => {
   return (
     <Layout>
       <div className="welcome">
+        <h3>Start Live Teaching</h3>
         <Input
           ref={inputRef}
           placeholder="What are you teaching today?"
           value={lectureName}
           onChange={(e) => setState(e.target.value)}
         />
-        <h3>Start Live Teaching</h3>
         <Button
           onClick={createLiveStream}
           disabled={!lectureName}
